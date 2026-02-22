b21a53cb4e8bb4821a78ce3a496c80035fdf655e
xolwzzz <xolwz@Outlook.com>
2026-02-22 00:28:00 -0500

    Enable threading mode for SocketIO and port config
    Updated SocketIO to use threading mode and added environment variable for port configuration.

--- NodeMonitor/server/app.py
+++ NodeMonitor/server/app.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit
 from datetime import datetime
 import uuid
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'changeme123'
-socketio = SocketIO(app, cors_allowed_origins="*")
+
+# Force threading mode - do NOT let it auto-detect eventlet
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
 
 clients = {}
 
@@ -48,4 +51,5 @@ def handle_request():
     emit('client_update', {'clients': list(clients.values())})
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
